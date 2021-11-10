977d11e2d6521f691ab5371eceddb7a41a9f4b56
Matthew Cook <matthewcook13987@gmail.com>
2021-11-10 16:08:38 -0600

    completes form component by adding selector to filter todos later

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <div>
-      <form>
+    
+    <form>
       <input type='text' className='todo-input'/>
       <button className='todo-btn' type='submit'>
         <i className='add-button'></i>
       </button>
+      <div className='select'>
+        <select name='todos' className='filter-todos'>
+          <option value='all'>All</option>
+          <option value='completed'>Completed</option>
+          <option value='uncompleted'>Failed</option>
+        </select>
+      </div>
 
-      </form>
-    </div>
+    </form>
+    
   )
 }
